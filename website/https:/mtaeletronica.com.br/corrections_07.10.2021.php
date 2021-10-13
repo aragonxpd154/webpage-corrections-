4926a5e701e30bd5cb7ad9bb9c73d2cebea0601d
Marcos Obel <marcos.silvabl@live.com>
2021-10-12 22:53:59 -0300

    UPDATE FIX 12.10.2021

--- website/https:/mtaeletronica.com.br/corrections_07.10.2021.php
+++ website/https:/mtaeletronica.com.br/corrections_07.10.2021.php
@@ -2,7 +2,7 @@ public function __construct() {
 	if (! empty($this->parent)) {
 		$this->parent->admin_notices[] = array(
 			'type'    => 'error',
-			'msg'     => '<strong>' . __( 'File Permission Issues', 'pinnacle' ) . '</strong><br/>' . sprintf( __( 'We were unable to modify required files. Please check your permissions, or modify your wp-config.php file to contain your FTP login credentials as <a href="%s" target="_blank">outlined here</a>.', 'pinnacle' ), 'https://codex.wordpress.org/Editing_wp-config.php#WordPress_Upgrade_Constants' ),
+			'msg'     => '<strong>' . __( 'File Permission Issues', 'pinnacle' ) . '</strong><br/>' . sprintf( __( 'We were unable to modify required files. Please check your permissions, or modify your wp-config.php file to contain your FTP login credentials as <a href="%s" target="_blank">outlined here</a>.', 'pinnacle' ), 'https://wordpress.org/support/article/editing-wp-config-php/' ),
 			'id'      => 'redux-wp-login',
 			'dismiss' => false,
 		);
